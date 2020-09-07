5ac73aee54c1b18f5b0de0f682269e1b4a32606f
yocuervoc <yocuervoc@unal.edu.co>
2020-09-07 16:00:07 -0500

    webpack

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+const Header = () =>{
+    const view = `
+
+    <div class="Header-main">
+        <div class="Header-logo">
+            <h1>
+                <a href="">100tifico</a>
+            </h1>
+        </div>
+        <div class="Header-nave">
+            <a href="#/about">about</a>
+        </div>
+    </div>
+    
+    `;
+    return view
+}
+
+export default Header
